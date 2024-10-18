cc0dc7b4e296de10ed152d17655bdeb2c216003b
isha <isharai8584@gmail.com>
2024-10-18 19:09:32 +0530

    Second program of cpp

---
+++ tp1.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+//# is gives a directery or pre proces directory include is a folder and iostream is file or header file and also we said library
+using namespace std;
+//namespace is scope that gives a place for standard
+int main(){                                                   
+     // it is a main fuction wher start our program and curly braces gives a startinfg end of the program.
+cout <<"hello everyone";
+
+return 0;  
+// ts said to th operating system our program has no error and it is not mendatory to write in program.
+}
